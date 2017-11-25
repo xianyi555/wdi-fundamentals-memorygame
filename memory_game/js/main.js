879b24f0f35aaf27bc7133e172643c30e8cddfe3
Sunny <sunnyyi@SunnydeMacBook-Air.local>
2017-11-24 20:17:57 -0800

    created game logic

---
+++ memory_game/js/main.js
@@ -0,0 +1,9 @@
+console.log("Up and running!");
+
+var cardOne = "Queen";
+var cardTwo = "Queen";
+var cardThree = "King";
+var cardFour = "King";
+
+console.log("User flipped " + cardOne);
+console.log("User flipped " + cardThree);
